383a69269d2e2244b385202bca4d56cd7cee11e7
BuildAutomationYash <cnaresh.jd@gmail.com>
2015-03-18 14:58:46 +0530

    MavenProject Created

---
+++ ExampleAPP/src/test/java/com/yash/MessageTest.java
@@ -0,0 +1,28 @@
+package com.yash;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MessageTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		String msg = "Hello";
+		Message m = new Message();
+		m.setMsg(msg);
+		assertEquals(msg, m.getMsg());
+
+	}
+
+}
